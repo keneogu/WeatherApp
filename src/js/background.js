df369f56e0ae2a5c9374b9d31d449b7dda71ed3d
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-05-11 23:36:44 +0100

    Add background.js file

---
+++ src/js/background.js
@@ -0,0 +1,31 @@
+import clearSky from '../images/clear.jpeg';
+import cloud from '../images/cloud.jpg';
+import rain from '../images/rain.jpg';
+import thunder from '../images/thunder.jpg';
+import snow from '../images/snow.jpg';
+
+function displayWeather() {
+    switch (data.weather[0].main) {
+      case 'Clear':
+        document.body.style.backgroundImage = `url(${clearSky})`;
+        break;
+      case 'Clouds':
+        document.body.style.backgroundImage = `url(${cloud})`;
+        break;
+      case 'Rain':
+      case 'Drizzle':
+      case 'Mist':
+        document.body.style.backgroundImage = `url(${rain})`;
+        break;
+      case 'Thunderstorm':
+        document.body.style.backgroundImage = `url(${thunder})`;
+        break;
+      case 'Snow':
+        document.body.style.backgroundImage = `url(${snow})`;
+        break;
+      default:
+        break;
+	}
+}
+
+export default displayWeather
