b4e80a53f639d16ad3be6376dbf3ee92383b5d3a
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-05-14 18:26:02 +0100

    Fix linters errors

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import displayWeather from './js/background';
 import changeTemp from './js/temp';
 
 const search = document.querySelector('.search-box');
-search.addEventListener('keypress', setAction);
 
 function setAction(e) {
-	if(e.keyCode == 13) {
-		getResult(search.value)
-		.then(data => displayWeather(data))
-		.then(search.value = "");
-	}
+  if (e.keyCode === 13) {
+    getResult(search.value)
+      .then((data) => displayWeather(data))
+      .then(search.value = '');
+  }
 }
+search.addEventListener('keypress', setAction);
 changeTemp();
--- src/js/weather.js
+++ src/js/weather.js
@@ -1,20 +1,20 @@
 import axios from 'axios';
 
 const getResult = async (query) => {
-	const key = 'd1b4490baf954b9a45f0e011f71bb87b';
-	const city = document.querySelector('.location .city');
-	const temp = document.querySelector('.current .temp');
-	const weather = document.querySelector('.current .weather');
-    try {
-		const res = await axios(`https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${key}&units=metric`);
-		const { data } = res;
-		city.innerHTML = `${data.name}, ${data.sys.country}`;
-		temp.innerHTML = `${Math.round(data.main.temp)}`;
-		weather.innerHTML = `${data.weather[0].main}`;
-		return data;
-	} catch (error) {
-		return error;
-	}
-}
+  const key = 'd1b4490baf954b9a45f0e011f71bb87b';
+  const city = document.querySelector('.location .city');
+  const temp = document.querySelector('.current .temp');
+  const weather = document.querySelector('.current .weather');
+  try {
+    const res = await axios(`https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${key}&units=metric`);
+    const { data } = res;
+    city.innerHTML = `${data.name}, ${data.sys.country}`;
+    temp.innerHTML = `${Math.round(data.main.temp)}`;
+    weather.innerHTML = `${data.weather[0].main}`;
+    return data;
+  } catch (error) {
+    return error;
+  }
+};
 
 export default getResult;
